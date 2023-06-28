e3639e1c2eedfa279f1fc3f9066b38b1fd73f31e
Lionel Herrero Battista <lionherrero@gmail.com>
2023-06-28 20:29:25 -0300

    End of section 8.

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,4 +28,17 @@ app.UseAuthorization();
 
 app.MapControllers(); // tells which request goes to were controller
 
+using var scope = app.Services.CreateScope(); // gives access to all services
+var services = scope.ServiceProvider;
+try // we need to handle any exception
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run(); // runs app
